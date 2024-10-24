0e4ddc1f71c886cb32daafe026a3cafa9a2970b1
4nnoy1ng <daniel.tuertscher@student.htldornbirn.at>
2024-10-24 10:22:38 +0200

    Objektiv erstellt

---
+++ src/Lens.java
@@ -0,0 +1,15 @@
+public class Lens {
+    private int Focatlength;
+
+    public Lens() {
+        this.Focatlength = Focatlength;
+    }
+
+    public void setFocatlength(int focatlength) {
+        Focatlength = focatlength;
+    }
+
+    public int getFocatlength() {
+        return Focatlength;
+    }
+}
