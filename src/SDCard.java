22ebb3046d78b56665beda41a1ada0cd13a07ba5
4nnoy1ng <daniel.tuertscher@student.htldornbirn.at>
2024-10-24 10:18:22 +0200

    SDCard erstellt

---
+++ src/SDCard.java
@@ -0,0 +1,15 @@
+public class SDCard {
+    private int Capacity;
+
+    public SDCard(int Capacity) {
+        this.Capacity = Capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        Capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return Capacity;
+    }
+}
