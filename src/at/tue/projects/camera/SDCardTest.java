2acb5865e46f5b059e3e5036d90cdfb9556c3cba
4nnoy1ng <daniel.tuertscher@student.htldornbirn.at>
2024-11-07 11:49:39 +0100

    Versucht takePic einzufügen

--- src/at/tue/projects/camera/SDCardTest.java
+++ src/at/tue/projects/camera/SDCardTest.java
@@ -1,7 +1,10 @@
 package at.tue.projects.camera;
 
-import java.util.Date;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.lang.reflect.Array;
+import java.util.Date;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,19 +12,35 @@ import org.junit.Test;
 public class SDCardTest {
     private SDCard SDCard;
     Picture p1;
+    Picture p2;
 
     @Before
     public void setUp() {
         p1 = new Picture("Daniel", new Date(), 100);
+        p2 = new Picture("Ronny", new Date(), 200);
     }
 
     @Test
     public void testGetCapacity() {
         Assert.assertEquals(p1.getSize(), 100, 0.001);
+        Assert.assertEquals(p2.getSize(), 200, 0.001);
+    }
+
+    public class Camera {
+        private Camera Camera;
+        Picture p1;
+        Picture p2;
+
+        @Before
+    public void setUp() {
+        p1 = new Picture("Daniel", new Date(), 100);
+        p2 = new Picture("Ronny", new Date(), 200);
     }
 
     @Test
     public void testTakePicture() {
-
+        Assert.assertEquals(p1.takePicture(), 2);
+        
     }
+
 }
