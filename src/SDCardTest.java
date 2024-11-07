0d7084013a3ff9c11332bdd29723f28e09207d69
4nnoy1ng <daniel.tuertscher@student.htldornbirn.at>
2024-11-07 10:58:35 +0100

    Erster Test eingefügt

--- src/SDCardTest.java
+++ src/SDCardTest.java
@@ -1,16 +1,18 @@
 import java.util.Date;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 public class SDCardTest {
     @Test
     public void testGetCapacity() {
+        Pictures p1 = new Pictures("Daniel", new Date(), 100);
 
+        Assert.assertEquals(p1.getSize(), 100, 0.001);
     }
 
     @Test
     public void testTakePicture() {
-        Pictures p1 = new Pictures("Daniel", new Date(), 100);
 
-        Assert.assertE(p1.getSize() > 0);
     }
 }
