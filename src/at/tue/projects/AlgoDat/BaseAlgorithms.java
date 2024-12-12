d31fce2fd3e8e0f4b34d4b8a9e156d2f2b510ae8
4nnoy1ng <daniel.tuertscher@student.htldornbirn.at>
2024-12-12 11:21:28 +0100

    InvertArray

--- src/at/tue/projects/AlgoDat/BaseAlgorithms.java
+++ src/at/tue/projects/AlgoDat/BaseAlgorithms.java
@@ -1,5 +1,12 @@
 package at.tue.projects.AlgoDat;
 
 public class BaseAlgorithms {
-
+    public int[] revert(int[] data) {
+        int[] reversed = new int[data.length];
+        int j = 0;
+        for (int i = data.length - 1; i >= 0; i--) {
+            reversed[j++] = data[i];
+        }
+        return reversed;
+    }
 }
--- src/at/tue/projects/AlgoDat/BaseAlgorithmsTest.java
+++ src/at/tue/projects/AlgoDat/BaseAlgorithmsTest.java
@@ -1,5 +1,18 @@
 package at.tue.projects.AlgoDat;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+
 public class BaseAlgorithmsTest {
+
+    @Test
+    void testRevert() {
+        BaseAlgorithms algo = new BaseAlgorithms();
+        int[] data = { 1, 1, 2, 3, 4, 7 };
+        int[] expected = { 7, 4, 3, 2, 1, 1 };
+        assertArrayEquals(expected, algo.revert(data));
+        System.out.println(Arrays.toString(algo.revert(data)));
+    }
 
 }
